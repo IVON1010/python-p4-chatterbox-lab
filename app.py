4f435779f196c9ce81a555e3ab0702b72907b659
YvonneNdungu <ndunguyvonne10@gmail.com>
2024-07-15 13:18:07 +0300

    commit changes

---
+++ app.py
@@ -0,0 +1,78 @@
+
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+from models import db, Message
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "chatterbox"
+
+@app.route('/messages', methods=["GET", "POST"])
+def messages():
+    if request.method == "GET":
+        messages = Message.query.order_by(Message.created_at).all()
+        messages_list = [message.to_dict() for message in messages]
+        return jsonify(messages_list), 200
+    
+    elif request.method == "POST":
+        data = request.get_json()
+        body = data.get("body")
+        username = data.get("username")
+
+        if not body or not username:
+            return make_response(jsonify({"error": "Both body and username should be provided"}), 400)
+
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+        return jsonify(message_dict), 201
+
+
+
+@app.route('/messages/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def message_by_id(id):
+    message = Message.query.filter_by(id=id).first()
+
+    if not message:
+        return make_response(jsonify({"error": "Message not found."}), 404)
+
+    if request.method == "GET":
+        message_dict = message.to_dict()
+        return jsonify(message_dict), 200
+    
+    elif request.method == "PATCH":
+        data = request.get_json()
+        new_body = data.get("body")
+
+        if not new_body:
+            return make_response(jsonify({"error": "The 'body' parameter is required to update the message"}), 400)
+
+        message.body = new_body
+        db.session.commit()
+
+        updated_message_dict = message.to_dict()
+        return jsonify(updated_message_dict), 200
+    
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        return jsonify({"message": "Message successfully deleted"}), 200
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
